785b3f3f3cd22ab4022997ce682e7fd7b76ec379
samettopcu88 <samet.topcu.88@icloud.com>
2024-10-26 17:32:11 +0300

    İlk sınıflar oluşturuldu.

---
+++ Villa.Entity/Entites/Banner.cs
@@ -0,0 +1,9 @@
+﻿namespace Villa.Entity.Entites
+{
+    public class Banner : BaseEntity
+    {
+        public string City { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

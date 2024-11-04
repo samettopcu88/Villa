b2e951c90b8a2c82886ab787c30a701812686dea
samettopcu88 <samet.topcu.88@icloud.com>
2024-11-04 21:19:33 +0300

    BannerController ve Banner viewsleri yapıldı.

--- Villa.WebUI/Controllers/BannerController.cs
+++ Villa.WebUI/Controllers/BannerController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using Villa.Business.Abstract;
 using Villa.DTO.Dtos.BannerDtos;
+using Villa.Entity.Entites;
 
 namespace Villa.WebUI.Controllers
 {
@@ -21,6 +23,42 @@ namespace Villa.WebUI.Controllers
             var values = await _bannerService.TGetListAsync();
             var bannerList = _mapper.Map<List<ResultBannerDto>>(values);
             return View(bannerList);
+        }
+
+        public async Task<IActionResult> DeleteBanner(ObjectId id)
+        {
+            await _bannerService.TDeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CreateBanner()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+        {
+            var newBanner = _mapper.Map<Banner>(createBannerDto);
+
+            await _bannerService.TCreateAsync(newBanner);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> UpdateBanner(ObjectId id)
+        {
+            var value = await _bannerService.TGetByIdAsync(id);
+
+            var updateBanner = _mapper.Map<UpdateBannerDto>(value);
+            return View(updateBanner);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBanner(UpdateBannerDto updateBannerDto)
+        {
+            var banner = _mapper.Map<Banner>(updateBannerDto);
+            await _bannerService.TUpdateAsync(banner);
+            return RedirectToAction("Index");
         }
     }
 }
